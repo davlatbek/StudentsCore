3c81c8cd5abb9c03120239d915cb658ba0d5d506
davlatbek <idavlatbek19@gmail.com>
2017-06-14 12:04:19 +0300

    extract to methods

--- src/utilities/Serializer.java
+++ src/utilities/Serializer.java
@@ -1,9 +1,6 @@
-package com.innopolis;
+package utilities;
 
-import appcore.Contact;
-import appcore.ContactType;
-import appcore.Lesson;
-import appcore.Student;
+import appcore.*;
 
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -32,53 +29,15 @@ public class Serializer {
     }
 
     private void serialize(List<Object> objects) throws UnsupportedEncodingException, XMLStreamException, FileNotFoundException, IllegalAccessException, NoSuchFieldException {
-        OutputStream outputStream = new FileOutputStream(file);
-        XMLStreamWriter out = XMLOutputFactory.newInstance().createXMLStreamWriter(
-                new OutputStreamWriter(outputStream, "utf-8"));
-
         out.writeStartDocument();
         out.writeCharacters("\n");
         out.writeStartElement("objects");
-
         for (Object object : objects){
             out.writeCharacters("\n\t");
             out.writeStartElement("object");
             out.writeAttribute("type", objects.get(0).getClass().getSimpleName());
-
-            for (Field field : object.getClass().getDeclaredFields()){
-                if (Modifier.isTransient(field.getModifiers())) continue;
-                field.setAccessible(true);
-                Class<?> c = field.getType();
-                if (c.isArray()){
-
-                }
-                out.writeCharacters("\n\t\t");
-                out.writeStartElement("field");
-                out.writeAttribute("type", field.getType().toString());
-                String s = field.get(object).toString();
-                out.writeAttribute("name", field.getName());
-                out.writeAttribute("value", s);
-                System.out.println(field.get(object).toString());
-                out.writeEndElement();
-            }
-
-            for (Method method : object.getClass().getDeclaredMethods()){
-                out.writeCharacters("\n\t\t");
-                out.writeStartElement("method");
-                out.writeAttribute("returntype", method.getReturnType().toString());
-                out.writeAttribute("name", method.getName());
-
-                for (Class<?> parameter : method.getParameterTypes()){
-                    out.writeCharacters("\n\t\t\t");
-                    out.writeStartElement("args");
-                    out.writeAttribute("type", parameter.getTypeName().toString());
-                    out.writeAttribute("name", parameter.getName());
-                    out.writeEndElement();
-                }
-                out.writeCharacters("\n\t\t");
-                out.writeEndElement();
-            }
-
+            writeFields(object);
+            writeMethods(object);
             out.writeCharacters("\n\t");
             out.writeEndElement();
             out.writeCharacters("\n");
@@ -88,13 +47,38 @@ public class Serializer {
         out.close();
     }
 
-    private void writeFields(Object obj) throws XMLStreamException {
-        for (Field field : obj.getClass().getDeclaredFields()){
+    private void writeMethods(Object object) throws XMLStreamException {
+        for (Method method : object.getClass().getDeclaredMethods()){
+            out.writeCharacters("\n\t\t");
+            out.writeStartElement("method");
+            out.writeAttribute("returntype", method.getReturnType().toString());
+            out.writeAttribute("name", method.getName());
+
+            for (Class<?> parameter : method.getParameterTypes()){
+                out.writeCharacters("\n\t\t\t");
+                out.writeStartElement("args");
+                out.writeAttribute("type", parameter.getTypeName().toString());
+                out.writeAttribute("name", parameter.getName());
+                out.writeEndElement();
+            }
+            out.writeCharacters("\n\t\t");
+            out.writeEndElement();
+        }
+    }
+
+    private void writeFields(Object object) throws XMLStreamException, IllegalAccessException {
+        for (Field field : object.getClass().getDeclaredFields()){
+            if (Modifier.isTransient(field.getModifiers())) continue;
             field.setAccessible(true);
+            Class<?> c = field.getType();
+
             out.writeCharacters("\n\t\t");
             out.writeStartElement("field");
             out.writeAttribute("type", field.getType().toString());
+            String s = field.get(object).toString();
             out.writeAttribute("name", field.getName());
+            out.writeAttribute("value", s);
+            System.out.println(field.get(object).toString());
             out.writeEndElement();
         }
     }
@@ -103,13 +87,10 @@ public class Serializer {
             UnsupportedEncodingException, XMLStreamException,
             IllegalAccessException, NoSuchFieldException {
         List<Object> objects = new ArrayList<>();
-        List<Contact> contacts = new ArrayList<>();
-        contacts.add(new Contact(ContactType.EMAIL, "d.isroilov@innopolis.ru"));
+        Group group = new Group(6L, 01, Semester.SPRING);
         Student student = new Student("Mark", "Brown", "Kidman",
-                new Date(1994, 01, 01), 6L);
-
+                new Date(1994, 01, 01), 6L, group);
         Lesson lesson = new Lesson("android course", new Date(123456789L), new Date(1234567890L), "subject", "desc", "A. Pervushov");
-
         objects.add(student);
         objects.add(lesson);
         File file = new File("students.xml");
